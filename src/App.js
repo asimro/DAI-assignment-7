318be6cff7a9302aa9131fa7781668639b954d10
LaibaNasir-1 <82863816+LaibaNasir-1@users.noreply.github.com>
2021-06-26 14:46:55 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,92 +1,95 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Web3 from 'web3'
 import './App.css'
 import { simpleStorageAbi, contractAddress } from './abis'
+import {Button, Form} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const web3 = new Web3(Web3.givenProvider)
-const contract = new web3.eth.Contract(simpleStorageAbi, contractAddress);
 
-async function Transfer(add, val) {
-  const accounts = await window.ethereum.enable()
-  const account = accounts[0];
-  const result = await contract.methods.transfer(add, val).send({
-    from: account
-  })
-  console.log(result);
-}
 
 function App() {
-  const [address, setAddress] = useState('');
-  const [amount, setAmount] = useState('');
-  //const [account, setAccount] = useState('');
-  
-  
+  const [address, setAddress] = useState("");
+  let [amount, setAmount] = useState('');
+  const [accounts, setAccounts] = useState();
+  const [loadContract, setLoadContract] = useState()
+  const [web3, setWeb3] = useState(null)
+
+  window.ethereum.on('accountsChanged', function (accounts) {
+    // Time to reload your interface with accounts[0]!
+    setAccounts(accounts)
+  })
+
+
+  const connectWallet = async () => {
+    const web3 = new Web3(Web3.givenProvider);
+    await Web3.givenProvider.enable();
+    setWeb3(web3);
+    console.log(web3)
+    const contract = new web3.eth.Contract(simpleStorageAbi, contractAddress);
+    console.log("contract", contract)
+    console.log("contract", contract.methods)
+
+    setLoadContract(contract);
+    console.log("contract", typeof contract)
+    const uAccounts = await web3.eth.getAccounts();
+    console.log("accounts",typeof  uAccounts)
+    setAccounts(uAccounts);
+  }
+
+
+  const Transfer = async () => {
+    console.log("amount", amount, "address", address, accounts)
+    try {
+      if (loadContract !== undefined) {
+        amount = amount * 10e17
+        amount = amount.toString();
+        const result = await loadContract.methods.transfer(address, amount).send({
+          from: accounts[0]
+        })
+        console.log(result)
+      }
+    }
+    catch (error) {
+      console.log("error", error)
+    }
+  }
+
   return (
     <div className="App">
-      <form>
-        <label>Enter Recipient's Address</label><br></br>
-        <input 
-        type = "text" 
-        placeholder="0x123XXXXXXX" 
-        required
-        value = {address}
-        onChange = {(e) => setAddress(e.target.value)}
-        />
-        <br />
-        <label>Enter amount</label><br></br>
-        <input type = "text" 
-        placeholder="1.0000000000"
-        required
-        value = {amount}
-        onChange = {(e) => setAmount(e.target.value)}
-        /><br></br>
-        <button onClick = {() => Transfer(address, amount)}>Send</button>
-      </form>
+      <header className = 'App-header'>
+      <Form>
+        <Form.Group>
+          <Form.Label>Recipient Address</Form.Label><br></br>
+          <input
+            type="text"
+            placeholder="Add Recipient's Address"
+            required
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <br />
+        </Form.Group>
+        <Form.Group>
+          <label>Amount</label><br></br>
+          <input type="text"
+            placeholder="Enter Amount to be Transfered"
+            required
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          /><br></br>
+        </Form.Group>
+      </Form>
+      <br></br>
+      {
+        !web3 ?
+          <Button onClick={connectWallet}>Connect Wallet</Button>
+          :
+          <Button onClick={Transfer}>Send</Button>
+
+      }  
+      </header>   
     </div>
   );
 }
 
 export default App;
-//<button onClick = {() => func()}>Send</button>
-
-
-/* class App extends Component {
-  componentWillMount() {
-    this.TransferDAI(address,amount)
-  }
-
-  async TransferDAI(address, amount) {
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
-    const contract = new web3.eth.Contract(simpleStorageAbi, contractAddress);    
-    const gas = await contract.methods.transfer(address, amount).estimateGas();
-    const result = await contract.methods.transfer(address, amount).send({
-      from: this.state.account,
-      gas 
-    })
-    console.log(result);
-  }
-
-  constructor(props) {
-    super(props)
-    this.state = { account: '' }
-  }
- 
-  render() {
-    return (
-      <div className="container">
-        <h1>Dai Transfer</h1>
-        <p>Your account: {this.state.account}</p>
-        <form>
-        <label htmlFor="toAdd">Enter Recipient's Address</label><br></br>
-        <input type = "text" id = "toAdd" name = "toAdd" placeholder="0x123XXXXXXX"></input><br></br>
-
-        <label htmlFor="amount">Enter amount</label><br></br>
-        <input type = "text" id = "amount" name = "amount" placeholder="1.0000000000"></input><br></br>   
-      </form>
-      </div>
-    );
-  }
-}
- */
