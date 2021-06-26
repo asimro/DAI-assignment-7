ea378ba44a1749594c7da8dd3c11cde47c5bb156
Sayyid Mansoob Hasan <62698302+Mansoob@users.noreply.github.com>
2021-06-26 18:53:40 +0500

    Update App.js
    Typo in token corrected

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
         <Form.Group>
           <Form.Label><h1>Sayyid Mansoob Hasan</h1></Form.Label><br></br>
           <br></br>
-        <Form.Label><h2>DAI Toke_Transfer App</h2></Form.Label><br></br>
+        <Form.Label><h2>DAI Token_Transfer App</h2></Form.Label><br></br>
           <br></br>
           <Form.Label><h3>Recipient Address</h3></Form.Label><br></br>
           <input
